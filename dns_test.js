d5357a9a5bac1ab6008a3e95d80b2a02e4ea1a20
Peter H. Jin <peter@peterjin.org>
2022-07-30 23:23:22 -0400

    Initial commit

---
+++ dns_test.js
@@ -0,0 +1,63 @@
+const fake_dns = require('./universal-relay/fake_dns.js');
+const endpoint = require('./universal-relay/endpoint.js');
+const dns_helpers = require('./universal-relay/dns_helpers.js');
+const express = require('express');
+var domain_manager = dns_helpers.make_soa_ns_handler('apps-vm8.srv.peterjin.org. dns.peterjin.org. 1 10000 10000 10000 120', ['apps-vm8.srv.peterjin.org.'/*,'apps-vm16-alt.srv.peterjin.org.'*/]);
+domain_manager.addDomain('0.a.6.0.8.0.2.0.6.2.ip6.arpa');
+domain_manager.addDomain('208.161.23.in-addr.arpa');
+domain_manager.addDomain('as398565.net');
+domain_manager.addDomain('ipv6-things.com');
+domain_manager.addDomain('ipv6.bible');
+domain_manager.addDomain('peterjin.com');
+domain_manager.addDomain('rdns.peterjin.org');
+var acme_manager = dns_helpers.make_acme_challenge_handler();
+var pdns_app = express();
+var acme_app = express();
+acme_app.use(express.urlencoded());
+acme_manager.make_express_app(acme_app);
+var example_com_records_map = new Map();
+acme_manager.addKey('as398565.net');
+acme_manager.addKey('colors.ipv6-things.com');
+acme_manager.addKey('ipv6-things.com');
+acme_manager.addKey('ipv6.bible');
+acme_manager.addKey('misc.ipv6-things.com');
+acme_manager.addKey('peterjin.com');
+acme_manager.addKey('ptable.ipv6-things.com');
+acme_manager.addKey('scp.ipv6-things.com');
+example_com_records_map.set('_acme-challenge', acme_manager.getAcmeChallengeTXTFunc(['example.com', '*.example.com']));
+example_com_records_map.set('_domainkey|selector', [[{qtype: "TXT", content: "v=DKIM1; k=rsa; p="}], null]);
+example_com_records_map.set('_dmarc', [[{qtype: "TXT", content: "v=DMARC1; p=reject;"}], null]);
+example_com_records_map.set('', [[null, {qtype: 'A', content: '192.0.2.1'}, {qtype: 'AAAA', content: '2001:db8::1'}, {qtype: "MX", content: "0 mail.example.com."}, {qtype: "TXT", content: "v=spf1 ip4:192.0.2.0/24 -all"}], null]);
+var mapping = dns_helpers.make_lookup_mapping(example_com_records_map, null);
+var m = fake_dns.make_urelay_ip_domain_map(0x100000000000000n, function(domain_parts, ep, extra_args) {
+	if (extra_args[3] === 2) {
+		if (domain_manager.getSOANS(ep).length > 0) return {"PRESIGNED": ['0']};
+		return {};
+	}
+	let result = [];
+	result.push(...(domain_manager.getSOANS(ep)));
+	ep.getSubdomainsOfThen(['com', 'example'], Infinity, function (res, t) {
+		let r = mapping.lookup(res.join('|'));
+		if (r) {
+			result.push(...r.rrset);
+		}
+	});
+	ep.getSubdomainsOfThen(['arpa'], Infinity, function (res, t) {
+		let ip_result = -1n;
+		switch (res[0]) {
+			case 'in-addr':
+				ip_result = dns_helpers.handle_inaddr_arpa(res.slice(1));
+				break;
+			case 'ip6':
+				ip_result = dns_helpers.handle_ip6_arpa(res.slice(1));
+				break;
+		}
+		if (ip_result >= 0n) {
+			let rdns_ep = (new endpoint.Endpoint()).setIPBigInt(ip_result);
+		}
+	});
+	return result;
+}, {domainList: domain_manager.domainList, haveDomainMetadata: true});
+m.make_pdns_express_app(pdns_app, null, true);
+pdns_app.listen({host: '127.0.0.10', port: 8181});
+acme_app.listen('/home/henrie/gitprojects/universal-relay/test/acme.sock');
